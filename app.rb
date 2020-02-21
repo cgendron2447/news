21d16ec3e8dabf330522edee5602670174061279
cgendron2447 <curtis.gendron@gmail.com>
2020-02-21 03:43:24 +0000

    2/20

--- app.rb
+++ app.rb
@@ -9,14 +9,17 @@ before { puts "Parameters: #{params}" }
 # enter your Dark Sky API key here
 ForecastIO.api_key = "423d101d0b874efebdfe642a8a22a00a"
 
-forecast = ForecastIO.forecast(#lat,#long).to_hash
-
 get "/" do
-    results = Geocoder.search(params["q"])
-        lat_long = results.first.coordinates #=> [48.856614, 2.3522219] # latitude and longitude
-        "The coordinates of #{params["q"]} are: Latitude: #{lat_long[0]} , Longitude: #{lat_long[1]}" 
+   view "ask"
 end
 
 get "/news" do
-  # do everything else
+    results = Geocoder.search(params["q"])
+        lat_long = results.first.coordinates 
+        @latitude = lat_long[0]
+        @longitude = lat_long[1]
+        @forecast = ForecastIO.forecast(@latitude,@longitude).to_hash
+        @current_temp = @forecast["currently"]["apparentTemperature"]
+        @current_conditions = @forecast["currently"]["summary"]
+        view "response"
 end
